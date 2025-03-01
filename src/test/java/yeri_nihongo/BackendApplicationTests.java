41f0755a6d866f9dbdd3349d3dfe439fddf052ac
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-01 16:54:01 +0900

    Initial commit

---
+++ src/test/java/yeri_nihongo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yeri_nihongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
