4fb395527173aa5515ceb522aa8e2d9e368ad75e
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-03 17:47:24 +0900

    CHORE: 공통 로직 분리(TimeTable 조회)

--- src/main/java/yeri_nihongo/common/service/CommonService.java
+++ src/main/java/yeri_nihongo/common/service/CommonService.java
@@ -14,10 +14,13 @@ import yeri_nihongo.exception.course.CourseNotFoundException;
 import yeri_nihongo.exception.enrollment.EnrollmentNotFoundException;
 import yeri_nihongo.exception.member.UserNotFoundException;
 import yeri_nihongo.exception.review.ReviewNotFoundException;
+import yeri_nihongo.exception.time.TimeTableNotFoundException;
 import yeri_nihongo.member.domain.Member;
 import yeri_nihongo.member.repository.MemberRepository;
 import yeri_nihongo.review.domain.Review;
 import yeri_nihongo.review.repository.ReviewRepository;
+import yeri_nihongo.time.domain.TimeTable;
+import yeri_nihongo.time.repository.TimeTableRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +31,7 @@ public class CommonService {
     private final CourseInfoRepository courseInfoRepository;
     private final EnrollmentRepository enrollmentRepository;
     private final ReviewRepository reviewRepository;
+    private final TimeTableRepository timeTableRepository;
 
     @Transactional(readOnly = true)
     public Member getMemberByMemberId(Long id) {
@@ -52,12 +56,18 @@ public class CommonService {
     public Course getCourseByCourseId(Long courseId) {
         return courseRepository.findById(courseId)
                 .orElseThrow(() -> new CourseNotFoundException(courseId)
-        );
+                );
     }
 
     @Transactional(readOnly = true)
     public CourseInfo getCourseInfoByCourseInfoId(Long courseInfoId) {
         return courseInfoRepository.findById(courseInfoId)
                 .orElseThrow(() -> new CourseInfoNotFoundException(courseInfoId));
+    }
+
+    @Transactional(readOnly = true)
+    public TimeTable getTimeTableByTimeTableId(Long timeTableId) {
+        return timeTableRepository.findById(timeTableId)
+                .orElseThrow(() -> new TimeTableNotFoundException(timeTableId));
     }
 }
