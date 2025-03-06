5f9990627598f19e18f53d52c955d78a53028c37
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-06 19:41:13 +0900

    CHORE: Projection 반환하지 않고 객체 반환하도록 코드 개선

--- src/main/java/yeri_nihongo/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/yeri_nihongo/enrollment/repository/EnrollmentRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import yeri_nihongo.course.dto.response.CourseInfoProjection;
+import yeri_nihongo.course.domain.CourseInfo;
 import yeri_nihongo.enrollment.domain.Enrollment;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
@@ -17,11 +18,11 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
             "WHERE e.member.id = :memberId")
     List<Enrollment> findEnrollmentByMemberId(@Param("memberId") Long memberId);
 
-    @Query("SELECT ci.title AS title, ci.mainImageUrl AS mainImageUrl " +
+    @Query("SELECT ci " +
             "FROM Enrollment e " +
             "JOIN e.timeTable tt " +
             "JOIN tt.course c " +
             "JOIN c.courseInfo ci " +
             "WHERE e.id = :enrollmentId")
-    CourseInfoProjection findCourseInfoByEnrollmentId(@Param("enrollmentId") Long enrollmentId);
+    Optional<CourseInfo> findCourseInfoByEnrollmentId(@Param("enrollmentId") Long enrollmentId);
 }
