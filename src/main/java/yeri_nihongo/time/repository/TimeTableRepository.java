96cfeb568ad18fff28f0f8669a81166a4b38faca
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-07 21:33:43 +0900

    FEAT: TimeTable로 CourseInfo 반환 메서드 구현

--- src/main/java/yeri_nihongo/time/repository/TimeTableRepository.java
+++ src/main/java/yeri_nihongo/time/repository/TimeTableRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import yeri_nihongo.course.domain.CourseInfo;
 import yeri_nihongo.time.domain.TimeTable;
 
 import java.util.List;
@@ -20,4 +21,11 @@ public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
             "FROM TimeTable tt " +
             "WHERE tt.id = :timeTableId")
     int findStudentCountByTimeTableId(@Param("timeTableId") Long timeTableId);
+
+    @Query("SELECT ci " +
+            "FROM TimeTable tt " +
+            "JOIN Course c ON tt.course.id = c.id " +
+            "JOIN CourseInfo ci ON c.courseInfo.id = ci. id " +
+            "WHERE tt.id = :timeTableId")
+    CourseInfo findCourseInfoByTimeTableId(@Param("timeTableId") Long timeTableId);
 }
