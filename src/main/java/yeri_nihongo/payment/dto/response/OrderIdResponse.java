7d5c6ad6339a72adc59af53cba30564d72ee4806
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-09 23:24:50 +0900

    CHORE: orderId 발급 dto 작성

---
+++ src/main/java/yeri_nihongo/payment/dto/response/OrderIdResponse.java
@@ -0,0 +1,11 @@
+package yeri_nihongo.payment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderIdResponse {
+
+    private String orderId;
+}
