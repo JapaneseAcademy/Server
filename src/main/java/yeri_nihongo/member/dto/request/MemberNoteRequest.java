d59115ab91b733b62edb17939c2f386f53194965
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-03-19 15:16:03 +0900

    CHORE: 학생 특이사항 수정 dto 작성

---
+++ src/main/java/yeri_nihongo/member/dto/request/MemberNoteRequest.java
@@ -0,0 +1,11 @@
+package yeri_nihongo.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberNoteRequest {
+
+    private String note;
+}
